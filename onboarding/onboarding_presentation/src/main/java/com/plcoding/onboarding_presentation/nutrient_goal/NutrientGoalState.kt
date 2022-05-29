cb943681ad59be1da14ed3b8a7b441efd64e67b9
박찬준 <chanjun.park@navercorp.com>
2022-05-29 12:35:56 +0900

    Expandable Meal Ui

--- onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/nutrient_goal/NutrientGoalState.kt
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/nutrient_goal/NutrientGoalState.kt
@@ -2,6 +2,6 @@ package com.plcoding.onboarding_presentation.nutrient_goal
 
 data class NutrientGoalState(
 	val carbsRatio: String = "40",
-	val proteinRatio: String = "40",
+	val proteinRatio: String = "30",
 	val fatRatio: String = "30"
 )
